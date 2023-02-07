fd4ab0551379d103acf98bd94165a8a5e99ac9e6
wlq <602093449@qq.com>
2023-02-07 20:27:13 +0800

    7. add react state and render student data to front-end

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,14 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 import { getAllStudents } from './client';
+import React, { Component } from 'react';
 
-function App() {
-  getAllStudents().then(res => res.json().then(students => {
-    console.log(students);
-  }));
-  return (
-    <h1>Hello SpringBoot And React</h1>
-  );
+class App extends Component{
+
+  constructor() {
+    super();
+    //定义state
+    this.state = {
+      students: []
+    };
+  }
+
+  //定义获取学生的方法
+  fetchStudents = () => {
+    getAllStudents()
+      .then(res => res.json()
+      .then(students => {
+        console.log(students)
+        this.setState({
+          students
+        })
+    }))
+  };
+  componentDidMount() {
+    this.fetchStudents();
+  }
+
+  render() {
+    const { students } = this.state
+    if(students && students.length) {
+      return students.map((student, index) => {
+        return (
+          <div key ={index}>
+            <h2>{student.studentId}</h2>
+            <p>{student.firstName}</p>
+            <p>{student.lastName}</p>
+            <p>{student.gender}</p>
+            <p>{student.email}</p>
+          </div>
+        )
+      })
+    }
+    return (
+      <h1>Hello SpringBoot And React</h1>
+    );
+  }
 }
 
 export default App;
