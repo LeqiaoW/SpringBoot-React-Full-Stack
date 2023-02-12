7b06dfac0b08faf56649ddfef4509caaa0bc2494
wlq <602093449@qq.com>
2023-02-12 21:15:21 +0800

    16. get student from database in dao service

--- src/main/kotlin/com/example/demo/student/StudentDataAccessService.kt
+++ src/main/kotlin/com/example/demo/student/StudentDataAccessService.kt
@@ -1,15 +1,48 @@
 package com.example.demo.student
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.RowMapper
 import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository// 定义为数据层
-class StudentDataAccessService {
+class StudentDataAccessService(
+        @Autowired private val jdbcTemplate: JdbcTemplate//引入 JdbcTemplate 进行与数据库交互
+) {
 
     fun getAllStudents(): List<Student>{
-        return listOf(
-                Student(UUID.randomUUID(), "James", "Bond", "JamesBond@gmail.com", Gender.MALE),
-                Student(UUID.randomUUID(), "Elisa", "Tamara", "ElisaTamara@gmail.com", Gender.FEMALE)
-        )
+        val sql: String = "" +
+                "SELECT " +
+                "student_id, " +
+                "first_name, " +
+                "last_name, " +
+                "email, " +
+                "gender " +
+                "FROM student"
+
+        return jdbcTemplate.query(sql, RowMapper<Student> { resultSet, i ->
+            //resultSet是查询到的结果集
+            // i 代表该数据的下标，是结果集的第几个数据
+            val studentIdStr = resultSet.getString("student_id")
+            val studentId = UUID.fromString(studentIdStr)
+            val firstName = resultSet.getString("first_name")
+            val lastName = resultSet.getString("last_name")
+            val email = resultSet.getString("email")
+            val gender = Gender.valueOf(resultSet.getString("gender").uppercase())
+
+            //将数据库数据转化为 Student类
+            Student(
+                    studentId = studentId,
+                    firstName = firstName,
+                    lastName = lastName,
+                    email = email,
+                    gender = gender
+            )
+        })
+//        return listOf(
+//                Student(UUID.randomUUID(), "James", "Bond", "JamesBond@gmail.com", Gender.MALE),
+//                Student(UUID.randomUUID(), "Elisa", "Tamara", "ElisaTamara@gmail.com", Gender.FEMALE)
+//        )
     }
 }
